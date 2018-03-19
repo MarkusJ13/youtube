c004aa8fd17301e5454e547787c0aa7cca081bf5
MarkusJ13 <mail.hiteshjangid@gmail.com>
2018-03-20 02:05:30 +0530

    Channel Added

--- src/App.js
+++ src/App.js
@@ -4,15 +4,47 @@ import './App.css';
 
 import Iframe from 'react-iframe';
 
+import $ from 'jquery';
+
 class App extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			url: "https://www.youtube.com/embed/EqPtz5qN7HM"
+			url: "https://www.youtube.com/embed/EqPtz5qN7HM",
+			videos: []
 		}
 	}
 
+	componentDidMount(){
+		let self = this
+		$.ajax({
+			url: "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB8aPDUD2B2IZrawHJr0SOiwfchKsLpoEA&channelId=UCD0oMMnL78IdahI82FJdohg&part=snippet,id&order=date&maxResults=20",
+			success: function(result){
+	       		let videos = []
+	       		for(let i=0; i<result.items.length; i++){
+	       			videos.push(result.items[i].id.videoId)
+	       		}
+	       		self.setState({videos: videos})
+	   		}
+	   	});
+	}
+
 	render() {
+		let videos = this.state.videos.map((data, index) => {
+			return <div className="iframe-container"> 
+				<Iframe
+					key={"video" + index}
+					url={"https://www.youtube.com/embed/" + data}
+					width="450px"
+					height="450px"
+					id="myId"
+					className="myClassname"
+					display="initial"
+					position="relative"
+					allowFullScreen
+				/>
+			</div>
+		})
 		return (
 			<div className="App">
 				<div className="url-input">
@@ -24,16 +56,9 @@ class App extends Component {
 						onChange={function(text){this.setState({url: this.refs.url.value})}.bind(this)}
 					/>
 				</div>
-				
-				<Iframe url={"https://www.youtube.com/embed/" + this.state.url.substr(this.state.url.length - 11, 11)}
-					width="450px"
-					height="450px"
-					id="myId"
-					className="myClassname"
-					display="initial"
-					position="relative"
-					allowFullScreen
-				/>
+				<div className="video-container">
+					{videos}
+				</div>
 			</div>
 		);
 	}
